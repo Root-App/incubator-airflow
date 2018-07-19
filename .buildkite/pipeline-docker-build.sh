5e3498558510633cfec0a72d6aa9097a19b32eea
Mike Ascah <mike.ascah@joinroot.com>
2018-07-19 11:24:38 -0400

    Add buildkite config

---
+++ .buildkite/pipeline-docker-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+git clone ${AIRFLOW_PLUGIN_REPO}
+
+if [[ "$1" = "dev" ]]; then
+  docker build -t ${DEV_ECR_URI}:${BUILDKITE_COMMIT} .
+
+  docker tag ${DEV_ECR_URI}:${BUILDKITE_COMMIT} ${ECR_URI}:${BUILDKITE_BRANCH}
+
+  docker ${DEV_ECR_URI}:${BUILDKITE_COMMIT}
+  docker ${DEV_ECR_URI}:${BUILDKITE_BRANCH}
+fi
+
+if [[ "$1" = "production" ]]; then
+  docker pull ${DEV_ECR_URI}:${BUILDKITE_COMMIT}
+
+  docker tag ${DEV_ECR_URI}:${BUILDKITE_COMMIT} ${PROD_ECR_URI}:${BUILDKITE_COMMIT}
+  docker tag ${DEV_ECR_URI}:${BUILDKITE_COMMIT} ${PROD_ECR_URI}:${BUILDKITE_BRANCH}
+
+  docker push ${PROD_ECR_URI}:${BUILDKITE_COMMIT}
+  docker push ${PROD_ECR_URI}:${BUILDKITE_BRANCH}
+fi
