12e1d6a8214c13e5f5a41b0ffc5ce63814c29f76
Mike Ascah <mike.ascah@joinroot.com>
2018-08-29 08:58:51 -0400

    Remove reverse proxy config from old flask app setup

--- airflow/www/app.py
+++ airflow/www/app.py
@@ -42,18 +42,7 @@ csrf = CSRFProtect()
 
 
 def create_app(config=None, testing=False):
-    class ReverseProxied(object):
-        def __init__(self, app):
-            self.app = app
-
-        def __call__(self, environ, start_response):
-            scheme = environ.get('HTTP_X_FORWARDED_PROTO')
-            if scheme:
-                environ['wsgi.url_scheme'] = scheme
-            return self.app(environ, start_response)
-
     app = Flask(__name__)
-    app.wsgi_app = ReverseProxied(app.wsgi_app)
     app.secret_key = configuration.conf.get('webserver', 'SECRET_KEY')
     app.config['LOGIN_DISABLED'] = not configuration.conf.getboolean(
         'webserver', 'AUTHENTICATE')
